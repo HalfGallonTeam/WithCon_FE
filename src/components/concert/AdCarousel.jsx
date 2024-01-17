75d00ca7111c7334e263de9e144f075fa5e385ab
Heo Jisoo <barley6m@gmail.com>
2024-01-17 22:49:14 +0900

    메인페이지 완성 1차

--- src/components/concert/AdCarousel.jsx
+++ src/components/concert/AdCarousel.jsx
@@ -1,5 +1,82 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const AdCarousel = () => {
-  return <div>광고가 들어갑니다.</div>;
+  const [activeNum, setActiveNum] = useState(0);
+
+  const adList = [
+    { title: "콘서트 광고", desc: "사랑해요 아이유", link: "ad-link-concert" },
+    { title: "콘서트 광고", desc: "사랑해요 아이유", link: "ad-link-concert" },
+    {
+      title: "뮤지컬 광고",
+      desc: "캣츠, 위키드, 아서왕의 전설",
+      link: "ad-link-musical",
+    },
+    {
+      title: "연극 광고",
+      desc: "학교 연극동아리 공짜 연극 사랑함. 정규공연 감사함.",
+      link: "ad-link-play",
+    },
+    {
+      title: "연극 광고",
+      desc: "학교 연극동아리 공짜 연극 사랑함. 정규공연 감사함. multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장multi-elipsis테스트용 긴 문장",
+      link: "ad-link-play",
+    },
+  ];
+  const maxLength = adList.length;
+  const movePercent = Math.floor(100 / maxLength);
+  const adCards = [];
+  const bullets = [];
+
+  adList.map((ad, index) => {
+    const className = index === activeNum ? "active" : "";
+    const card = (
+      <div
+        className={`slide ${className}`}
+        key={index}
+        style={{ width: `${movePercent}%` }}
+      >
+        <h3 className="ad-title">{ad.title}</h3>
+        <p className="ad-desc">{ad.desc}</p>
+        <div className="ad-link">
+          <Link to={ad.link}>
+            <button className="ad-link-button">바로가기</button>
+          </Link>
+        </div>
+      </div>
+    );
+    const bullet = <div key={`b${index}`} className={`bullet ${className}`} />;
+    adCards.push(card);
+    bullets.push(bullet);
+  });
+
+  const slide = (num) => {
+    let newNum = activeNum + num;
+    if (newNum >= maxLength) newNum = 0;
+    if (newNum < 0) newNum = maxLength - 1;
+    setActiveNum(newNum);
+  };
+
+  return (
+    <div className="ad-carousel">
+      <div
+        className="ad-list"
+        style={{
+          width: `${maxLength * 100}%`,
+          transform: `translateX(-${activeNum * movePercent}%)`,
+        }}
+      >
+        {adCards}
+      </div>
+      <div className="ad-bullets">{bullets}</div>
+      <button className="ad-button-left" onClick={() => slide(-1)}>
+        &lt;
+      </button>
+      <button className="ad-button-right" onClick={() => slide(1)}>
+        &gt;
+      </button>
+    </div>
+  );
 };
 
 export default AdCarousel;
--- src/components/concert/ConLists.jsx
+++ src/components/concert/ConLists.jsx
@@ -1,63 +1,100 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import AdCarousel from "./AdCarousel";
 
-const ConcertCard = () => {
+const concertInfos = [
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 몬테크리스토",
+    place: "충무아트센터 대극장",
+    date: "2023.11.21-2024.2.25",
+    reservation: "좌석우위",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 레미제라블",
+    place: "블루스퀘어 신한카드홀",
+    date: "2023.11.21-2024.3.10",
+    reservation: "단독판매",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "태양의서커스 루치아 - 부산",
+    place: "신세계 센텀시티 내 빅탑",
+    date: "2024.4.14-2.4",
+    reservation: "",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 몬테크리스토",
+    place: "충무아트센터 대극장",
+    date: "2023.11.21-2024.2.25",
+    reservation: "좌석우위",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 레미제라블",
+    place: "블루스퀘어 신한카드홀",
+    date: "2023.11.21-2024.3.10",
+    reservation: "단독판매",
+  },
+];
+
+const ConcertCard = (props) => {
+  const [likethis, setLikethis] = useState(false);
+  const navigate = useNavigate();
+  const info = props.info;
+  const className =
+    info.reservation === "단독판매"
+      ? "hot"
+      : info.reservation === ""
+      ? "hidden"
+      : "";
+  const likeChange = (e) => {
+    e.stopPropagation();
+    setLikethis(!likethis);
+  };
+
   return (
-    <Link to="/thisConcert/">
-      <div className="concert-card">
-        <img
-          className="concert-poster"
-          src="https://dummyimage.com/160x240/E6E6E6/0011ff"
-          alt="빈 이미지"
-        />
-        <div className="concert-mini-info">
-          <div className="info-top-line">
-            <p className="concert-title">공연이름</p>
-            <i className="bi bi-heart"></i>
-            <i className="bi bi-heart-fill"></i>
-            <button className="like">7</button>
-          </div>
-          <div className="info-bottom-line">
-            <p className="concert-place">공연위치</p>
-            <p className="concert-date">2024-01-01</p>
-          </div>
+    <div className="concert-card" onClick={() => navigate(info.title)}>
+      <div className="poster-box">
+        <img className="concert-poster" src={info.poster} alt={info.title} />
+      </div>
+      <div className="concert-mini-info">
+        <div className="info-top-line">
+          <h3 className="concert-title">{info.title}</h3>
+          <button className="like" onClick={likeChange}>
+            {likethis && <i className="bi bi-heart-fill"></i>}
+            {!likethis && <i className="bi bi-heart"></i>}
+          </button>
+        </div>
+        <div className="info-bottom-line">
+          <p className="concert-place">{info.place}</p>
+          <p className="concert-date">{info.date}</p>
+          <p className={`reservation-tip ${className}`}>{info.reservation}</p>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
 const ConLists = () => {
+  const concertCards = [];
+  concertInfos.map((info) => {
+    concertCards.push(<ConcertCard info={info} />);
+  });
   return (
     <>
       <div className="container">
         <AdCarousel />
         <h2 className="concert-category">콘서트</h2>
-        <div className="concert-list">
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-        </div>
+        <div className="concert-list">{concertCards}</div>
 
         <h2 className="concert-category">뮤지컬</h2>
-        <div className="concert-list">
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-        </div>
+        <div className="concert-list">{concertCards}</div>
 
         <h2 className="concert-category">연극</h2>
-        <div className="concert-list">
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-          <ConcertCard />
-        </div>
+        <div className="concert-list">{concertCards}</div>
       </div>
     </>
   );
