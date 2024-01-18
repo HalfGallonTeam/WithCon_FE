36e21a8ce3e18ebc1973dc5fe80508135e772078
bidanee <dleksql119@gmail.com>
2024-01-19 01:12:57 +0900

    myPage ui

--- src/components/mypage/Mypage.jsx
+++ src/components/mypage/Mypage.jsx
@@ -2,20 +2,27 @@ import { Outlet, Link } from "react-router-dom";
 
 const MyPage = () => {
   return (
-    <>
+    <div className="mypage-container">
       <div>
-        <button>
-          <Link to="/mypage/">찜한공연</Link>
-        </button>
-        <button>
-          <Link to="/mypage/mychat/">내 채팅방</Link>
-        </button>
-        <button>
+        <div className="mypage-title">
+          <h1>마이 페이지</h1>
+        </div>
+        <div className="btn-container">
+          <button className="btn-mid">
+            <Link to="/mypage/">찜한공연</Link>
+          </button>
+          <button className="btn-mid second-btn">
+            <Link to="/mypage/mychat/">내 채팅방</Link>
+          </button>
+        </div>
+        {/* <button>
           <Link to="/mypage/profile/">프로필 변경</Link>
-        </button>
+        </button> */}
       </div>
-      <Outlet />
-    </>
+      <div className="mypage-like-list">
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
