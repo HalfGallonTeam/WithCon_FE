cf0f83e7a59066a121cbc14d8c458a40dc211b9a
Heo Jisoo <barley6m@gmail.com>
2024-01-18 19:23:44 +0900

    메인페이지 가로스크롤 설정

--- src/components/concert/ConLists.jsx
+++ src/components/concert/ConLists.jsx
@@ -1,8 +1,77 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
-import AdCarousel from "./AdCarousel";
 
 const concertInfos = [
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 몬테크리스토",
+    place: "충무아트센터 대극장",
+    date: "2023.11.21-2024.2.25",
+    reservation: "좌석우위",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 레미제라블",
+    place: "블루스퀘어 신한카드홀",
+    date: "2023.11.21-2024.3.10",
+    reservation: "단독판매",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "태양의서커스 루치아 - 부산",
+    place: "신세계 센텀시티 내 빅탑",
+    date: "2024.4.14-2.4",
+    reservation: "",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 몬테크리스토",
+    place: "충무아트센터 대극장",
+    date: "2023.11.21-2024.2.25",
+    reservation: "좌석우위",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 레미제라블",
+    place: "블루스퀘어 신한카드홀",
+    date: "2023.11.21-2024.3.10",
+    reservation: "단독판매",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 몬테크리스토",
+    place: "충무아트센터 대극장",
+    date: "2023.11.21-2024.2.25",
+    reservation: "좌석우위",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 레미제라블",
+    place: "블루스퀘어 신한카드홀",
+    date: "2023.11.21-2024.3.10",
+    reservation: "단독판매",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "태양의서커스 루치아 - 부산",
+    place: "신세계 센텀시티 내 빅탑",
+    date: "2024.4.14-2.4",
+    reservation: "",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 몬테크리스토",
+    place: "충무아트센터 대극장",
+    date: "2023.11.21-2024.2.25",
+    reservation: "좌석우위",
+  },
+  {
+    poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
+    title: "뮤지컬 레미제라블",
+    place: "블루스퀘어 신한카드홀",
+    date: "2023.11.21-2024.3.10",
+    reservation: "단독판매",
+  },
   {
     poster: "https://dummyimage.com/180x240/E6E6E6/0011ff",
     title: "뮤지컬 몬테크리스토",
@@ -78,22 +147,37 @@ const ConcertCard = (props) => {
   );
 };
 
-const ConLists = () => {
+const ConLists = (props) => {
+  let { category } = useParams();
+  const categoryToText = {
+    concert: "콘서트",
+    musical: "뮤지컬",
+    play: "연극",
+  };
+
+  const length = props.length;
+  //concertInfos는 length가 있으면 main page용으로 5개 끊고, 아닌 경우 page length로 끝장보기.
+  let newConcertInfos = [];
+  if (length) {
+    for (let i = 0; i < length; i++) {
+      newConcertInfos.push(concertInfos[i]);
+    }
+  } else {
+    newConcertInfos = concertInfos;
+  }
+  //concertInfo를 불러오는 단계에서 데이터 총량을 결정하고, 카드 그리기는 .map함수로 끝까지 진행할 것.
+
   const concertCards = [];
-  concertInfos.map((info) => {
-    concertCards.push(<ConcertCard info={info} />);
+  newConcertInfos.map((info, index) => {
+    concertCards.push(<ConcertCard info={info} key={index} />);
   });
+
   return (
     <>
       <div className="container">
-        <AdCarousel />
-        <h2 className="concert-category">콘서트</h2>
-        <div className="concert-list">{concertCards}</div>
-
-        <h2 className="concert-category">뮤지컬</h2>
-        <div className="concert-list">{concertCards}</div>
-
-        <h2 className="concert-category">연극</h2>
+        {category && (
+          <h2 className="concert-category">{categoryToText[category]}</h2>
+        )}
         <div className="concert-list">{concertCards}</div>
       </div>
     </>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,14 +2,42 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      <p>로그인 페이지</p>
-      <button>
-        <Link to="/Signup/">회원가입하기</Link>
-      </button>
-      <button>
-        <Link to="/findpassword/">비밀번호찾기</Link>
-      </button>
+    <div className="middle-container">
+      <div className="container">
+        <div className="login-box">
+          <h1 className="title">위드콘</h1>
+          <form className="login-form">
+            <table>
+              <tbody>
+                <tr>
+                  <td>아이디</td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>비밀번호</td>
+                  <td>
+                    <input type="password" />
+                  </td>
+                </tr>
+                <tr className="login-button-box">
+                  <td colSpan="2">
+                    <button className="login-button">로그인하기</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </form>
+          <button>
+            <Link to="/Signup/">회원가입하기</Link>
+          </button>
+          <button>
+            <Link to="/findpassword/">비밀번호찾기</Link>
+          </button>
+          <hr aria-hidden="true" />
+        </div>
+      </div>
     </div>
   );
 };
