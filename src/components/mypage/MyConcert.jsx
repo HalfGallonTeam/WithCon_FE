cdcdba708a6239c7439eb4ec96a40a817065cf58
bidanee <dleksql119@gmail.com>
2024-01-21 23:31:51 +0900

    profile UI수정 footer email추가

--- src/components/mypage/MyConcert.jsx
+++ src/components/mypage/MyConcert.jsx
@@ -1,40 +1,9 @@
-import { Link } from "react-router-dom";
 import ConcertCard from "../concert/ConcertCard";
 import { concertInfos as info } from "../concert/ConLists";
 
-{
-  /**
-const ConcertCard = () => {
-  return (
-    <Link to="/thisConcert/">
-      <div className="concert-card">
-        <img
-          className="concert-poster"
-          src="https://dummyimage.com/160x240/E6E6E6/0011ff"
-          alt="빈 이미지"
-        />
-        <div className="concert-mini-info">
-          <div className="info-top-line">
-            <p className="concert-title">공연이름</p>
-            <i className="bi bi-heart"></i>
-            <i className="bi bi-heart-fill"></i>
-            <button className="like">7</button>
-          </div>
-          <div className="info-bottom-line">
-            <p className="concert-place">공연위치</p>
-            <p className="concert-date">2024-01-01</p>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
-}; 
-*/
-}
-
 const MyConcert = () => {
   return (
-    <div>
+    <div className="mypage-like-list">
       <div className="concert-list">
         <ConcertCard info={info[0]} />
         <ConcertCard info={info[1]} />
