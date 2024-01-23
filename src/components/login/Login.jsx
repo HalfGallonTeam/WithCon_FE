8a16507ed36308434e8552cca4d8f6e6f893f602
Heo Jisoo <barley6m@gmail.com>
2024-01-24 01:50:38 +0900

    테스트 merge용 커밋

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,33 +5,14 @@ import naverBtn from "../../assets/images/naver-login.png";
 
 const naverLogin = () => {
   const ClientID = "EbpEEUuGnuKMtgt1URzI";
-  const callbackURL = "https://monumental-maamoul-ad99a8.netlify.app/";
+  const callbackURL = "https://withcon.netlify.app/";
   var naver_id_login = new window.naver_id_login(ClientID, callbackURL);
-  //팝업 형태의 로그인 인증
-  naver_id_login.setPopup();
-  //callback url과 로그인 버튼이 있는 url의 subdomain 불일치 문제 해결
-  //상태 토큰은 !!!"cookie로 전달"!!!!하므로, 양쪽 domain에서 cookie를 참조해야 함.
-  //상태 토큰 비교를 위한 domain 설정
+  //서비스와 callback url의 subdomain 불일치 문제 해결. 상태 토큰 비교를 위한 domain 설정
   naver_id_login.setDomain(".service.com");
   var state = naver_id_login.getUniqState();
   naver_id_login.setState(state);
   naver_id_login.init_naver_id_login();
-  console.log(naver_id_login.getAccessToken());
-  /**
-    <!-- callback페이지 처리 스크립트 -->
-    <script type="text/javascript">
-      // 네이버 사용자 프로필 조회 이후 프로필 정보를 처리할 callback function
-      function naverSignInCallback() {
-        // naver_id_login.getProfileData('프로필항목명');
-        // 프로필 항목은 개발가이드를 참고하시기 바랍니다.
-        alert(naver_id_login.getProfileData('email'));
-        alert(naver_id_login.getProfileData('nickname'));
-        alert(naver_id_login.getProfileData('age'));
-      }
-      // 네이버 사용자 프로필 조회
-      naver_id_login.get_naver_userprofile("naverSignInCallback()");
-    </script>
-*/
+
   return;
 };
 
