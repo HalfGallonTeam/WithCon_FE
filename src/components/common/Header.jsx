650e3b1b8e763c3e5b11b185f74db92fb9f1168b
Heo Jisoo <barley6m@gmail.com>
2024-01-25 13:02:43 +0900

    조정7차

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -18,31 +18,35 @@ const Header = () => {
   //네이버 소셜 로그인을 바탕으로 토큰 받아오기.
   const [accessToken, setAccessToken] = useRecoilState(access_token);
   useEffect(() => {
-    var naver_id_login = new window.naver_id_login(
-      VITE_NAVER_CLIENT_ID,
-      NAVER_CALLBACK_URL
-    );
-    const naverAccessToken = naver_id_login.oauthParams.access_token;
-    if (naverAccessToken) {
-      console.log("naver_access_token", naverAccessToken);
-      console.log(
-        "네이버 access token이 존재하므로, 이 토큰을 backend에 POST로 전달합니다. 그리고 response를 받아, '위드콘'의 access token과 refresh token을 전역 상태(recoil atom)에 저장합니다. 이 과정은 보내는 데이터가 id 및 pw가 아닐 뿐 로그인 동작과 동일할 것으로 예상됩니다."
-      );
-      setAccessToken(() => "가공된" + naverAccessToken);
-      if (accessToken) {
-        localStorage.setItem("withcon_token", JSON.stringify(accessToken));
-        window.alert("로컬스토리지에 토큰이 저장되었습니다.");
-      }
+    //로그인을 판단함
+    let withconToken = localStorage.getItem("withcon_token");
+    if (withconToken) {
+      setIsLogin(true);
     } else {
-      console.log("naver access token이 없습니다");
+      //로그인이 안 되었을 때 네이버로그인을 판단함.
+      var naver_id_login = new window.naver_id_login(
+        VITE_NAVER_CLIENT_ID,
+        NAVER_CALLBACK_URL
+      );
+      const naverAccessToken = naver_id_login.oauthParams.access_token;
+      if (naverAccessToken) {
+        console.log("naver_access_token", naverAccessToken);
+        console.log(
+          "네이버 access token이 존재하므로, 이 토큰을 backend에 POST로 전달합니다. 그리고 response를 받아, '위드콘'의 access token과 refresh token을 전역 상태(recoil atom)에 저장합니다. 이 과정은 보내는 데이터가 id 및 pw가 아닐 뿐 로그인 동작과 동일할 것으로 예상됩니다."
+        );
+        setAccessToken(() => "가공된" + naverAccessToken);
+        localStorage.setItem(
+          "withcon_token",
+          JSON.stringify("가공된" + naverAccessToken)
+        );
+        window.alert("로컬스토리지에 토큰이 저장되었습니다.");
+        setIsLogin(true);
+      } else {
+        console.log("naver_access_token이 없습니다");
+      }
     }
   }, []);
 
-  //로그인을 판단함
-  let withconToken = localStorage.getItem("withcon_token");
-  if (withconToken) {
-    setIsLogin(true);
-  }
   //로그아웃을 실행함
   const logoutFunc = () => {
     setIsLogin(false);
