d19e70df5aac3a6261f0855338534e4ed9fc489b
Heo Jisoo <barley6m@gmail.com>
2024-01-29 00:16:58 +0900

    AdCarousel 수정

--- src/components/concert/MainPage.jsx
+++ src/components/concert/MainPage.jsx
@@ -4,33 +4,35 @@ import ConLists from "./ConLists";
 
 const MainPage = () => {
   return (
-    <div className="container">
+    <>
       <AdCarousel />
-      <h2 className="concert-category">
-        <Link to="/performance/concert">콘서트</Link>
-      </h2>
-      <div className="main-carousel-container">
-        <div className="responsible-carousel scroll-x">
-          <ConLists length="5" />
+      <div className="container">
+        <h2 className="concert-category">
+          <Link to="/performance/concert">콘서트</Link>
+        </h2>
+        <div className="main-carousel-container">
+          <div className="responsible-carousel scroll-x">
+            <ConLists length="5" />
+          </div>
+        </div>
+        <h2 className="concert-category">
+          <Link to="/performance/musical">뮤지컬</Link>
+        </h2>
+        <div className="main-carousel-container">
+          <div className="responsible-carousel scroll-x">
+            <ConLists length="5" />
+          </div>
+        </div>
+        <h2 className="concert-category">
+          <Link to="/performance/play">연극</Link>
+        </h2>
+        <div className="main-carousel-container">
+          <div className="responsible-carousel scroll-x">
+            <ConLists length="5" />
+          </div>
         </div>
       </div>
-      <h2 className="concert-category">
-        <Link to="/performance/musical">뮤지컬</Link>
-      </h2>
-      <div className="main-carousel-container">
-        <div className="responsible-carousel scroll-x">
-          <ConLists length="5" />
-        </div>
-      </div>
-      <h2 className="concert-category">
-        <Link to="/performance/play">연극</Link>
-      </h2>
-      <div className="main-carousel-container">
-        <div className="responsible-carousel scroll-x">
-          <ConLists length="5" />
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
