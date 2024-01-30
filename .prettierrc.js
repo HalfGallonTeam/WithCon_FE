8fc3f1f30efb3e9c5fc7082cc02c60afd2631e7a
Heo Jisoo <barley6m@gmail.com>
2024-01-30 14:34:14 +0900

    prettier 올리기

---
+++ .prettierrc.js
@@ -0,0 +1,47 @@
+{
+	"workbench.colorTheme": "Material Theme Ocean",
+	"liveSassCompile.settings.autoprefix": [],
+	"liveSassCompile.settings.formats": [
+	  {
+		"format": "expanded",
+		"extensionName": ".css",
+		"savePath": "./src/assets/css"
+	  }
+	],
+	"liveSassCompile.settings.generateMap": false,
+	"json.schemas": [],
+	//주석 색깔 설정
+	"editor.tokenColorCustomizations": {
+	  "comments": "#fff"
+	},
+	"editor.fontSize": 16,
+	"editor.insertSpaces": false,
+	"liveServer.settings.CustomBrowser": "chrome",
+	"launch": {
+	  "configurations": [],
+	  "compounds": []
+	},
+	// ESLINT
+	"eslint.validate": [
+	  "html",
+	  "javascript",
+	  "javascriptreact",
+	  "typescript",
+	  "typescriptreact"
+	],
+	"eslint.alwaysShowStatus": true,
+	// 수정 후 저장할 때 eslint로 autofix 실행 (ex. let => const)
+	"editor.codeActionsOnSave": {
+	  "source.fixAll.eslint": "explicit"
+	},
+	"editor.defaultFormatter": "esbenp.prettier-vscode",
+	//언어별로 프리티어 자동 지정.
+	"[javascript]": {
+	  "editor.formatOnSave": true
+	},
+	"[javascriptreact]": {
+	  "editor.formatOnSave": true
+	},
+	"liveSassCompile.settings.forceBaseDirectory": "",
+	"window.zoomLevel": 2
+  }  
