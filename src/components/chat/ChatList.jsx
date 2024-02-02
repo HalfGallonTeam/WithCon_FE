df250edf56ac1b7ee2edee0ebd1dee9aa9032cff
bidanee <dleksql119@gmail.com>
2024-02-02 13:01:14 +0900

    ✨ chatlist hashtag 검색 기능 구현

--- src/components/chat/ChatList.jsx
+++ src/components/chat/ChatList.jsx
@@ -3,11 +3,15 @@ import { useLocation } from "react-router-dom";
 import ChatRoom from "./ChatRoom";
 import CreateChatRoom from "./CreateChatRoom";
 import Paging from "../common/Paging";
+import axios from "axios";
 
 const ChatList = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalCount, setTotalCount] = useState(65);
+  const [searchHashtag, setSearchHashtag] = useState("");
+  const [data, setData] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
   const url = useLocation();
   useEffect(() => {
     setCurrentPage(new URLSearchParams(url.search).get("page") || 1);
@@ -19,33 +23,75 @@ const ChatList = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleHashTagChange = (e) => {
+    setSearchHashtag(e.target.value);
+  };
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const server = "http://localhost:8000/chatRooms";
+        const response = await axios.get(server);
+        setData(response.data);
+      } catch (error) {
+        console.error("데이터오류", error);
+      }
+    };
+    getData();
+  }, []);
+  const upDateFilterData = () => {
+    const filterData = data
+      ? searchHashtag
+        ? data.filter((data) => data.tags.includes(searchHashtag))
+        : data
+      : null;
+    setFilteredData(filterData);
+  };
+  const handleSearch = () => {
+    upDateFilterData();
+  };
+  const handleSearchKeydown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="chat-list-container">
-      <h1>채팅방 목록</h1>
-      <div className="hashtag-search">
-        <input id="hashtag" placeholder="해시태그를 입력해 주세요." />
-        <button>검색</button>
-      </div>
-      <div className="chat-btn-container">
-        <div className="check">
-          <input type="checkbox" id="checkroom" />
-          <label htmlFor="checkroom">
-            <span>마감된 방 보지 않기</span>
-          </label>
+      <div className="chat-list-header">
+        <h1>채팅방 목록</h1>
+
+        <div className="header-right">
+          <div className="hashtag-search">
+            <input
+              id="hashtag"
+              placeholder="해시태그를 입력해 주세요."
+              value={searchHashtag}
+              onChange={handleHashTagChange}
+              onKeyDown={handleSearchKeydown}
+            />
+            <button onClick={handleSearch}>검색</button>
+          </div>
+          <div className="chat-btn-container">
+            <div className="create-chat">
+              <button className="create-chat-btn" onClick={openModal}>
+                채팅방 만들기
+              </button>
+            </div>
+            {isModalOpen && <CreateChatRoom onClose={closeModal} />}
+          </div>
         </div>
-        <div className="create-chat">
-          <button className="create-chat-btn" onClick={openModal}>
-            채팅방 만들기
-          </button>
-        </div>
-        {isModalOpen && <CreateChatRoom onClose={closeModal} />}
       </div>
       <div className="list-container">
-        <ChatRoom />
-        <ChatRoom />
-        <ChatRoom />
-        <ChatRoom />
-        <ChatRoom />
+        {data && (filteredData || data).length > 0 ? (
+          (filteredData || data).map((searchData) => (
+            <ChatRoom searchData={searchData} key={searchData.id} />
+          ))
+        ) : (
+          <div className="room-msg-container">
+            <span>채팅방이 없습니다.</span>
+          </div>
+        )}
       </div>
       <Paging totalCount={totalCount} currentPage={currentPage} />
     </div>
