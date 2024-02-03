7d4062f4080af7f6519c4d2bca26cfed61541f66
bidanee <dleksql119@gmail.com>
2024-02-03 14:16:25 +0900

    🔨 회원가입 1차수정

--- src/components/login/Validate.js
+++ src/components/login/Validate.js
@@ -2,7 +2,8 @@ const validationRegex = {
   userId: /^[a-zA-Z0-9]{2,12}$/,
   password: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/,
   email: /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,}$/,
-  nickname: /^[A-Za-z0-9ㄱ-힣]{2,6}$/,
+  nickname: /^[A-Za-z0-9ㄱ-힣]{2,10}$/,
+  phone: /^010-\d{3,4}-\d{4}$/,
 };
 
 export const validateInput = (type, value) => {
