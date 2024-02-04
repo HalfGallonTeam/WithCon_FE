85aaea3e0d07fd397296a64a6259e95e4f4dae6e
Heo Jisoo <barley6m@gmail.com>
2024-02-05 02:35:19 +0900

    🚧 채팅방 초기설정을 json-server에서 불러오는 것으로 바꿈. 테스트 완료.

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,12 +1,24 @@
-import { useNavigate } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import instance from "../../assets/constants/instance";
 
-const member = [
-  ["one", "1"],
-  ["two", "2"],
-  ["three", "3"],
-];
+//컴포넌트 리렌더링을 막기 위한 조치
+const basic = {
+  chatRoomId: 12,
+  roomName: "토요일 콘서트 같이 가자요... 제발요",
+  userCount: "1",
+  members: [
+    {
+      email: "tester@test.com",
+      username: "abcd7787",
+      password: "password",
+      loginType: "HOME",
+      nickName: "위콘2",
+      phoneNumber: "010-7774-4567",
+    },
+  ],
+};
 
 const Chat = () => {
   const [toggle, setToggle] = useState(false);
@@ -16,6 +28,8 @@ const Chat = () => {
     e.stopPropagation();
     setToggle(true);
   };
+
+  //토글 동작 함수
   useEffect(() => {
     const toggleClose = (e) => {
       if (toggleRef.current && !toggleRef.current.contains(e.target)) {
@@ -27,13 +41,65 @@ const Chat = () => {
       document.removeEventListener("click", toggleClose);
     };
   }, []);
+
+  //주소창 기반으로 채팅방 정보를 불러옵니다.
+  const { concertTitle, id } = useParams();
+  const [parentPerformance, setParentPerformance] =
+    useState("아이유 드론쇼 콘서트");
+  const [chatInitial, setChatInitial] = useState(basic);
+
+  useEffect(() => {
+    const enterChatRoom = async () => {
+      try {
+        //부모공연 이름 세팅
+        const responseTitle = await instance.get(
+          `/performances/${concertTitle}`
+        );
+        const dataTitle = await responseTitle.data["title"];
+        setParentPerformance(dataTitle);
+        //채팅방 초기설정
+        //const response = await instance.get(`/chatRoom/${id}/enter`);
+        const response = await instance.get("chatRoomEnter/1");
+        const datas = await response.data;
+        setChatInitial(datas);
+      } catch (error) {
+        console.error(error, "에러");
+      }
+    };
+    enterChatRoom();
+  }, []);
+
+  const exitChatroom = async () => {
+    try {
+      const response = await instance.delete(`/chatRoom/${id}/exit`);
+      if (response.status === 204) {
+        window.alert("채팅방에서 퇴장했습니다.");
+        navigate(`/title/${concertTitle}/chat`);
+      }
+    } catch (error) {
+      console.error(error, "에러");
+      window.alert("서버 응답 불일치, 채팅방에서 퇴장했습니다.");
+      navigate(`/title/${concertTitle}/chat`);
+    }
+  };
+
+  const chatMembers = [];
+  chatInitial.members.map((member) => {
+    chatMembers.push(
+      <li key={member.username} className="member-info">
+        <div className="member-img">{member.username}</div>
+        <div className="member-name">{member.nickName}</div>
+      </li>
+    );
+  });
+
   return (
     <div className="chat-container">
       <div className="chat-wrap">
         <div className="chat-header">
           <div className="chat-room-name">
-            <h1>아이유 드론쇼 콘서트</h1>
-            <h2>토요일 콘서트 같이 가자요... 제발요</h2>
+            <h1>{parentPerformance}</h1>
+            <h2>{chatInitial.roomName}</h2>
           </div>
           <button className="toggle" onClick={toggleOpen}>
             <GiHamburgerMenu size={20} />
@@ -47,20 +113,10 @@ const Chat = () => {
                 <div className="title" onClick={() => navigate("/")}>
                   위드콘
                 </div>
-
-                <ul className="member-lists">
-                  {member.map((member) => {
-                    return (
-                      <li key={member[1]} className="member-info">
-                        <div className="member-img">{member[1]}</div>
-                        <div className="member-name">{member[0]}</div>
-                      </li>
-                    );
-                  })}
-                </ul>
+                <ul className="member-lists">{chatMembers}</ul>
               </div>
               <div className="chat-exit">
-                <button>채팅방 나가기</button>
+                <button onClick={exitChatroom}>채팅방 나가기</button>
               </div>
             </div>
           )}
