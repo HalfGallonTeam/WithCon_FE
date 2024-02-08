ffdeab8e4bd36fdbce146cbe1c0fbc23fc69bc95
bidanee <dleksql119@gmail.com>
2024-02-08 13:50:45 +0900

    🔨 test색상추가

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -1,21 +1,45 @@
-const ButtonModal = ({ text, buttonContainer, button1, button2, onClick }) => {
+const ButtonModal = ({
+  text,
+  buttonContainer,
+  button1,
+  button2,
+  onClickButton1,
+  onClickButton2,
+  textColor,
+}) => {
   return (
     <div className="modal-container">
       <div className="text-container">
-        <span className="modal-text">{text}</span>
+        <span
+          className={`modal-text" ${textColor ? `modal-${textColor}` : ""} `}
+        >
+          {text}
+        </span>
       </div>
       {buttonContainer === "2" ? (
         <div className="button-container">
-          <button type="button" className="modal-button" onClick={onClick}>
+          <button
+            type="button"
+            className="modal-button"
+            onClick={onClickButton1}
+          >
             {button1}
           </button>
-          <button type="button" className="modal-button" onClick={onClick}>
+          <button
+            type="button"
+            className="modal-button"
+            onClick={onClickButton2}
+          >
             {button2}
           </button>
         </div>
       ) : buttonContainer === "1" ? (
         <div className="button-container">
-          <button type="button" className="modal-button" onClick={onClick}>
+          <button
+            type="button"
+            className="modal-button single"
+            onClick={onClickButton1}
+          >
             {button1}
           </button>
         </div>
