6df0c2bb40e1c7b3757dd36766a94b2686aa67e3
bidanee <dleksql119@gmail.com>
2024-02-08 13:09:35 +0900

    modal생성

---
+++ src/components/common/modal.jsx
@@ -0,0 +1,27 @@
+const ButtonModal = ({ text, buttonContainer, button1, button2 }) => {
+  return (
+    <div className="modal-container">
+      <div className="text-container">
+        <span className="modal-text">{text}</span>
+      </div>
+      {buttonContainer === "2" ? (
+        <div className="button-container">
+          <button type="button" className="modal-button">
+            {button1}
+          </button>
+          <button type="button" className="modal-button">
+            {button2}
+          </button>
+        </div>
+      ) : buttonContainer === "1" ? (
+        <div className="button-container">
+          <button type="button" className="modal-button">
+            {button1}
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default ButtonModal;
