1b7bc69bfff98de064c8fcf81e0db7a7b4561472
bidanee <dleksql119@gmail.com>
2024-02-15 09:40:59 +0900

    👽️  kakaoLogin api 수정

--- src/components/login/KakaoLogin.jsx
+++ src/components/login/KakaoLogin.jsx
@@ -1,33 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import instance from "../../assets/constants/instance";
+import axios from "axios";
 
 const KakaoLogin = () => {
   const code = new URL(window.location.href).searchParams.get("code");
   const navigate = useNavigate();
-  let isRequestingToken = false;
-  const getToken = async () => {
-    if (isRequestingToken) return;
-    try {
-      isRequestingToken = true;
-      const response = await instance.post("/auth/oauth2/login", {
-        registrationId: "kakao",
-        authorizationCode: code,
-      });
-      const data = await response.data;
-      if (data.accessToken) {
-        localStorage.setItem("withcon_token", data.accessToken);
-        navigate("/");
-      }
-    } catch (error) {
-      console.error("잘못된 접근입니다.", error);
-    }
-    isRequestingToken = false;
-  };
+  let isRequestingToken = true;
   useEffect(() => {
+    const getToken = async () => {
+      if (!isRequestingToken) {
+        console.log("이미 존재");
+        return;
+      }
+      isRequestingToken = false;
+      if (!code) {
+        console.log("코드가 없습니다.");
+        navigate("/");
+        return;
+      }
+      try {
+        const response = await axios.post("/api/auth/oauth2/login", {
+          registrationId: "kakao",
+          authorizationCode: code,
+        });
+        const accessToken = await response.headers.authorization;
+        if (accessToken) {
+          localStorage.setItem("withcon_token", JSON.stringify(accessToken));
+          navigate("/");
+        }
+      } catch (error) {
+        console.error("잘못된 접근입니다.", error);
+      }
+    };
     getToken();
   }, [code]);
-  console.log(code);
 };
 
 export default KakaoLogin;
