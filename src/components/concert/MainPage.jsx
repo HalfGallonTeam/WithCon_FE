6936589c14406eb8472eab7ff986576631953a0f
Heo Jisoo <barley6m@gmail.com>
2024-02-15 19:55:25 +0900

    🎨 메인페이지 베스트공연

--- src/components/concert/MainPage.jsx
+++ src/components/concert/MainPage.jsx
@@ -4,64 +4,81 @@ import instance from "../../assets/constants/instance";
 import AdCarousel from "./AdCarousel";
 import ConcertCard from "./ConcertCard";
 
-const ConListBests = (props) => {
-  const [infos, setInfos] = useState([]);
-  const category = props.category;
-  const categoryKR = {
-    concert: "콘서트",
-    musical: "뮤지컬",
-    play: "연극",
-  };
+const MainPage = () => {
+  const [concert, setConcert] = useState([]);
+  const [musical, setMusical] = useState([]);
+  const [play, setPlay] = useState([]);
+  let loading = false;
 
   useEffect(() => {
-    const getInfos = async () => {
+    const getPerformanceBest = async () => {
+      if (loading) return;
+      loading = true;
       try {
-        const response = await instance.get(
-          `/performance-best?category=${props.category}`
-        );
+        const response = await instance.get("/performance/best");
         const datas = await response.data;
-        setInfos(datas);
+        const integer = datas.Integer;
+        const res = datas.PerformanceResponse;
+        console.log(datas);
+
+        setConcert(res.splice(0, integer[0]));
+        setMusical(res.splice(0, integer[1]));
+        setPlay(res.splice(0, integer[2]));
       } catch (error) {
         console.error(error, "에러");
       }
     };
-    getInfos();
+    getPerformanceBest();
   }, []);
 
-  const concertCards = [];
-  infos.map((info, index) => {
-    concertCards.push(<ConcertCard info={info} key={index} />);
+  const concertArr = [];
+  const musicalArr = [];
+  const playArr = [];
+  concert.map((info, index) => {
+    concertArr.push(<ConcertCard info={info} key={index} />);
   });
-
-  return (
-    <>
-      <h2 className="concert-category">
-        <Link to={`/performance?category=${category}`}>
-          {categoryKR[category]}
-        </Link>
-      </h2>
-      <div className="main-carousel-container">
-        <div className="responsible-carousel scroll-x">
-          <div className="container">
-            <div className="concert-list">{concertCards}</div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-const MainPage = () => {
-  const categories = ["concert", "musical", "play"];
-  const bestLists = [];
-  categories.map((category) => {
-    bestLists.push(<ConListBests category={category} key={category} />);
+  musical.map((info, index) => {
+    musicalArr.push(<ConcertCard info={info} key={index} />);
+  });
+  play.map((info, index) => {
+    playArr.push(<ConcertCard info={info} key={index} />);
   });
 
   return (
     <>
       <AdCarousel />
-      <div className="container">{bestLists}</div>
+      <div className="container">
+        <h2 className="concert-category">
+          <Link to={"/performance?category=concert"}>콘서트</Link>
+        </h2>
+        <div className="main-carousel-container">
+          <div className="responsible-carousel scroll-x">
+            <div className="container">
+              <div className="concert-list">{concertArr}</div>
+            </div>
+          </div>
+        </div>
+        <h2 className="concert-category">
+          <Link to={"/performance?category=musical"}>뮤지컬</Link>
+        </h2>
+        <div className="main-carousel-container">
+          <div className="responsible-carousel scroll-x">
+            <div className="container">
+              <div className="concert-list">{musicalArr}</div>
+            </div>
+          </div>
+        </div>
+        <h2 className="concert-category">
+          <Link to={"performance?category=play"}>연극</Link>
+        </h2>
+        <div className="main-carousel-container">
+          <div className="responsible-carousel scroll-x">
+            <div className="container">
+              <div className="concert-list">{playArr}</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
