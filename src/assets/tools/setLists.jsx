1b6fcfdac024b2427f9198b603b6ed2562b5438d
bidanee <dleksql119@gmail.com>
2024-02-28 15:17:12 +0900

    🎨 tag 검색 수정 파일입니다.

--- src/assets/tools/setLists.jsx
+++ src/assets/tools/setLists.jsx
@@ -1,10 +1,17 @@
 import instance from "../constants/instance";
 
-const setLists = async (request, setInfos, totalCount, setTotalCount) => {
+const setLists = async (
+  request,
+  setDatas,
+  setInfos,
+  totalCount,
+  setTotalCount
+) => {
   const response = await instance.get(request);
   console.log(response, "setLists");
   const datas = await response.data;
   setInfos(datas.content);
+  setDatas(datas.content);
   const length = datas.totalElements;
   if (length !== totalCount) {
     setTotalCount(length);
